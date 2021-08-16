ea44752847c8a4134fc30987cb116404bcaf8f8a
maksood-droid <maksood@alisonsgroup.com>
2021-08-16 22:32:35 +0530

    Login Page Completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,76 +22,103 @@ class _HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: new PreferredSize(
-        child: new Container(
-          padding: new EdgeInsets.only(
-              top: MediaQuery.of(context).padding.top
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: new PreferredSize(
+          child: new Container(
+            padding: new EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top
+            ),
+            decoration: new BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60),bottomRight:Radius.circular(60) ),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/rectangle_33.png'),
+                    fit: BoxFit.cover),
+            ),
+            child:Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 40,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25,right: 25),
+                    child: TabBar(
+                      unselectedLabelColor: white,
+                      isScrollable: false,
+
+
+                      labelColor: Colors.black,
+                      indicator: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: white),
+                      tabs: [
+
+                        Tab(
+                            child: Container(
+
+                          height: 40,
+                          decoration:BoxDecoration(
+                          border: Border.all(color: white),
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+
+                    ),
+                          child: Center(child: Text('Home')),)),
+                        Tab(child: Container(
+                          height: 40,
+                          decoration:BoxDecoration(
+                              border: Border.all(color: white),
+                              borderRadius: BorderRadius.all(Radius.circular(5)),
+
+                          ),child: Center(child: Text('Newsfeed')),)),
+                        Tab(child: Container(
+                          height: 40,
+                          decoration:BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(5)),
+                            border: Border.all(color: white),
+                          ),child: Center(child: Text('Gallery')),)),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40,)
+              ],
+            ),
           ),
-          decoration: new BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60),bottomRight:Radius.circular(60) ),
+          preferredSize: new Size(
+              MediaQuery.of(context).size.width,
+              150.0
+          ),
+        ),
+        backgroundColor: white,
+        bottomNavigationBar: Container(
+          height: 73,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(16),topRight:Radius.circular(16) ),
               image: DecorationImage(
                   image: AssetImage('assets/images/rectangle_33.png'),
                   fit: BoxFit.cover),
-          ),
+            ),
+          child: Center(child: Text('GIFT BOARD',style: style,)),
         ),
-        preferredSize: new Size(
-            MediaQuery.of(context).size.width,
-            100.0
-        ),
-      ),
-      backgroundColor: white,
-      bottomNavigationBar: Container(
-        height: 73,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(16),topRight:Radius.circular(16) ),
-            image: DecorationImage(
-                image: AssetImage('assets/images/rectangle_33.png'),
-                fit: BoxFit.cover),
-          ),
-        child: Center(child: Text('GIFT BOARD',style: style,)),
-      ),
-      body:
-      getFullView(),
+        body:
+        getFullView(),
 
+      ),
     );
   }
 
   Widget getFullView() {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // getTopContainer(),
-            getMiddleContainer(),
-            getBottomContainer(),
-            SizedBox(height: 60,)
-          ],
-        ),
-      ),
+    return TabBarView(
+      children: [
+        Icon(Icons.flight, size: 350),
+        Icon(Icons.directions_transit, size: 350),
+        Icon(Icons.directions_car, size: 350),
+      ],
     );
-  }
-  Widget getTopContainer(){
-    return Container(
-      height:150,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60),bottomRight:Radius.circular(60) ),
-        image: DecorationImage(
-            image: AssetImage('assets/images/rectangle_33.png'),
-            fit: BoxFit.cover),
-      ),
-      child: TabBar(
-        tabs: [
-          Tab(icon: Icon(Icons.flight)),
-          Tab(icon: Icon(Icons.directions_transit)),
-          Tab(icon: Icon(Icons.directions_car)),
-        ],
-      ),
-    );
+
   }
   Widget getMiddleContainer(){
     return Container(
@@ -187,88 +214,6 @@ class _HomeScreenState extends State<HomeScreen>{
           )
         ],
       ),
-    );
-  }
-  Widget getBottomContainer(){
-    return Container(
-      margin: EdgeInsets.only(left:30,right: 30 ),
-      child: Container(
-        child:
-        Column(
-          children: [
-            Row(
-              children: [
-              Container(
-                width: 3,
-                height: 65,
-                margin: EdgeInsets.only(top: 20),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/rectangle_10.png'),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-                Container(
-                  margin: EdgeInsets.only(left: 15,top: 20),
-                    width:MediaQuery.of(context).size.width-78,
-                    child: Text('Referral code is given to the person who has referred the prize money winner - 10%  of the winning amount can be claimed using the referral code which can  be input by editing the required details.',
-                      style: style2,
-                    ))
-              ],
-            ),
-            Row(
-              children: [
-              Container(
-                width: 3,
-                height: 35,
-                margin: EdgeInsets.only(top: 20),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/rectangle_10.png'),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-                Container(
-                  margin: EdgeInsets.only(left: 15,top: 20),
-                    width:MediaQuery.of(context).size.width-78,
-                    child: Text('The referral code can be applied only once - system to validate. ',
-                      style: style2,))
-              ],
-            ),
-            Row(
-              children: [
-              Container(
-                width: 3,
-                height: 35,
-                margin: EdgeInsets.only(top: 20),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/rectangle_10.png'),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-                Container(
-                  margin: EdgeInsets.only(left: 15,top: 20),
-                    width:MediaQuery.of(context).size.width-78,
-                    child: Text('The referral code in case not applicable can be skipped by inputting the required name and verified registered number.',
-                      style: style2,))
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-  Widget getTabs(){
-    return TabBarView(
-        children: [
-        Icon(Icons.flight, size: 350),
-    Icon(Icons.directions_transit, size: 350),
-    Icon(Icons.directions_car, size: 350),
-    ]
     );
   }
 
